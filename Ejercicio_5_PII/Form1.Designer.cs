cc25371dbe95e387c6d38e31b94edbf5c6ea6c0b
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2020-09-08 11:17:47 -0300

    primer commit

---
+++ Ejercicio_5_PII/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Ejercicio_5_PII
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.lstNombres = new System.Windows.Forms.ListBox();
+      this.lstPrecios = new System.Windows.Forms.ListBox();
+      this.lstCantidades = new System.Windows.Forms.ListBox();
+      this.lstGastosTotales = new System.Windows.Forms.ListBox();
+      this.btnCalcular = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.lblTotalGeneral = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // lstNombres
+      // 
+      this.lstNombres.FormattingEnabled = true;
+      this.lstNombres.ItemHeight = 25;
+      this.lstNombres.Location = new System.Drawing.Point(65, 128);
+      this.lstNombres.Name = "lstNombres";
+      this.lstNombres.Size = new System.Drawing.Size(311, 379);
+      this.lstNombres.TabIndex = 0;
+      // 
+      // lstPrecios
+      // 
+      this.lstPrecios.FormattingEnabled = true;
+      this.lstPrecios.ItemHeight = 25;
+      this.lstPrecios.Location = new System.Drawing.Point(425, 128);
+      this.lstPrecios.Name = "lstPrecios";
+      this.lstPrecios.Size = new System.Drawing.Size(311, 379);
+      this.lstPrecios.TabIndex = 0;
+      // 
+      // lstCantidades
+      // 
+      this.lstCantidades.FormattingEnabled = true;
+      this.lstCantidades.ItemHeight = 25;
+      this.lstCantidades.Location = new System.Drawing.Point(792, 128);
+      this.lstCantidades.Name = "lstCantidades";
+      this.lstCantidades.Size = new System.Drawing.Size(311, 379);
+      this.lstCantidades.TabIndex = 0;
+      // 
+      // lstGastosTotales
+      // 
+      this.lstGastosTotales.FormattingEnabled = true;
+      this.lstGastosTotales.ItemHeight = 25;
+      this.lstGastosTotales.Location = new System.Drawing.Point(1176, 128);
+      this.lstGastosTotales.Name = "lstGastosTotales";
+      this.lstGastosTotales.Size = new System.Drawing.Size(311, 379);
+      this.lstGastosTotales.TabIndex = 0;
+      // 
+      // btnCalcular
+      // 
+      this.btnCalcular.Location = new System.Drawing.Point(792, 558);
+      this.btnCalcular.Name = "btnCalcular";
+      this.btnCalcular.Size = new System.Drawing.Size(236, 77);
+      this.btnCalcular.TabIndex = 3;
+      this.btnCalcular.Text = "Calcular Gastos";
+      this.btnCalcular.UseVisualStyleBackColor = true;
+      this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(1171, 584);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(142, 25);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Total General";
+      // 
+      // lblTotalGeneral
+      // 
+      this.lblTotalGeneral.AutoSize = true;
+      this.lblTotalGeneral.Location = new System.Drawing.Point(1343, 584);
+      this.lblTotalGeneral.Name = "lblTotalGeneral";
+      this.lblTotalGeneral.Size = new System.Drawing.Size(19, 25);
+      this.lblTotalGeneral.TabIndex = 4;
+      this.lblTotalGeneral.Text = "-";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(65, 87);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(98, 25);
+      this.label3.TabIndex = 4;
+      this.label3.Text = "Nombres";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(425, 87);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(84, 25);
+      this.label4.TabIndex = 4;
+      this.label4.Text = "Precios";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(792, 87);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(121, 25);
+      this.label5.TabIndex = 4;
+      this.label5.Text = "Cantidades";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(1190, 87);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(157, 25);
+      this.label6.TabIndex = 4;
+      this.label6.Text = "Gastos Totales";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1552, 699);
+      this.Controls.Add(this.lblTotalGeneral);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.btnCalcular);
+      this.Controls.Add(this.lstGastosTotales);
+      this.Controls.Add(this.lstCantidades);
+      this.Controls.Add(this.lstPrecios);
+      this.Controls.Add(this.lstNombres);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox lstNombres;
+    private System.Windows.Forms.ListBox lstPrecios;
+    private System.Windows.Forms.ListBox lstCantidades;
+    private System.Windows.Forms.ListBox lstGastosTotales;
+    private System.Windows.Forms.Button btnCalcular;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label lblTotalGeneral;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label6;
+  }
+}
+
---
+++ Ejercicio_5_PII/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_5_PII
+{
+  public partial class Form1 : Form
+  {
+    private const int cantArray = 10;
+    private string[] aNombres = new string[cantArray];
+    private double[] aPrecios = new double[cantArray];
+    private int[] aCantidades = new int[cantArray];
+    private double[] aGastosTotales = new double[10];
+    private double totalGeneral;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      aNombres[0] = "Bananas";
+      aNombres[1] = "Manzanas";
+      aNombres[2] = "Peras";
+      aNombres[3] = "Naranjas";
+      aNombres[4] = "Ananas";
+      aNombres[5] = "Duraznos";
+      aNombres[6] = "Frutillas";
+      aNombres[7] = "Mandarinas";
+      aNombres[8] = "Uvas";
+      aNombres[9] = "Ciruelas";
+
+      aPrecios[0] = 35.65;
+      aPrecios[1] = 43.87;
+      aPrecios[2] = 35.45;
+      aPrecios[3] = 64.23;
+      aPrecios[4] = 27.94;
+      aPrecios[5] = 64.87;
+      aPrecios[6] = 34.98;
+      aPrecios[7] = 23.76;
+      aPrecios[8] = 34.98;
+      aPrecios[9] = 23.54;
+
+      aCantidades[0] = 5;
+      aCantidades[1] = 7;
+      aCantidades[2] = 5;
+      aCantidades[3] = 3;
+      aCantidades[4] = 4;
+      aCantidades[5] = 7;
+      aCantidades[6] = 8;
+      aCantidades[7] = 6;
+      aCantidades[8] = 8;
+      aCantidades[9] = 4;
+
+      for (int i = 0; i < cantArray; i++)
+      {
+        lstNombres.Items.Add(aNombres[i]);
+        lstPrecios.Items.Add($"$ {aPrecios[i]}");
+        lstCantidades.Items.Add(aCantidades[i]);
+      }
+    }
+
+    private void btnCalcular_Click(object sender, EventArgs e)
+    {
+      lstGastosTotales.Items.Clear();
+      totalGeneral = 0;
+      for (int i = 0; i < cantArray; i++)
+      {
+        aGastosTotales[i] = aPrecios[i] * aCantidades[i];
+        lstGastosTotales.Items.Add($"$ {Math.Round(aGastosTotales[i], 2)}"
+          );
+        totalGeneral += aGastosTotales[i];
+        lblTotalGeneral.Text = $"$ {Math.Round(totalGeneral, 2).ToString()}";
+      }
+    }
+  }
+}
